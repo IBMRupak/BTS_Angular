69d09b5554656631a8ef41b24e2d0f0f36f9ffcd
unknown <ibmjavanh20@iiht.tech>
2021-04-27 11:53:10 +0530

    iMplemented Status Flow

--- src/app/update-bug-form/update-bug-form.component.ts
+++ src/app/update-bug-form/update-bug-form.component.ts
@@ -10,6 +10,7 @@ export class UpdateBugFormComponent implements OnInit {
   title:String = 'UpdateBugForm';
   Bug:bug = new bug();
   bugArray: any;
+  currentStatus: any;
   constructor(private bugService: BugService) { }
   //get bug details to be prepopulated
   searchBugbyName(name: any) {
@@ -20,6 +21,7 @@ export class UpdateBugFormComponent implements OnInit {
       const observable = this.bugService.searchBugbyName1(bugname);
       observable.subscribe(response => {
         this.bugArray = response;
+        this.currentStatus= this.bugArray.status;
         console.log("success");
         if (this.bugArray) {
           this.Bug = this.bugArray;
@@ -47,7 +49,87 @@ export class UpdateBugFormComponent implements OnInit {
       alert('Bug is Updated')
 
     },
+
       error => {
+        if (this.currentStatus == 'NEW' && (this.Bug.status != 'NEW' || this.Bug.status != 'ASSIGNED')) {
+          alert('Status can be changed only to ASSIGNED');
+          return;
+        }
+        else if (this.currentStatus == 'ASSIGNED' && (this.Bug.status != 'ASSIGNED' || this.Bug.status != 'OPEN')) {
+          alert('Status can be changed only to OPEN');
+          return;
+        }
+        else if (this.currentStatus == 'OPEN' && (this.Bug.status != 'OPEN'
+         || this.Bug.status != 'DEFERRED' || this.Bug.status != 'DUPLICATE' ||
+          this.Bug.status != 'REJECTED' || this.Bug.status != 'NOT_A_BUG' ||
+          this.Bug.status != 'COULD_NOT_REPRODUCE' || this.Bug.status != 'NEED_MORE_INFO'
+           || this.Bug.status != 'WONT_FIX' || this.Bug.status != 'FIXED')) {
+          alert('Status can be changed only to DEFERRED, DUPLICATE, REJECTED, NOT_A_BUG, COULD_NOT_REPRODUCE, NEED_MORE_INFO, WONT_FIX, FIXED');
+          return;
+        }
+        else if (this.currentStatus == 'FIXED' && (this.Bug.status != 'FIXED' ||
+         this.Bug.status != 'PENDING_RETEST')) {
+          alert('Status can be changed only to PENDING_RETEST');
+          return;
+        }
+        else if (this.currentStatus == 'PENDING_RETEST' && (this.Bug.status
+           != 'PENDING_RETEST' || this.Bug.status != 'RETEST')) {
+          alert('Status can be changed only to RETEST');
+          return;
+        }
+        else if (this.currentStatus == 'RETEST' && (this.Bug.status != 'RETEST'
+         || this.Bug.status != 'REOPEN' || this.Bug.status != 'VERIFIED')) {
+          alert('Status can be changed only to REOPEN or VERIFIED');
+          return;
+        }
+        else if (this.currentStatus == 'REOPEN' && (this.Bug.status !=
+          'REOPEN' || this.Bug.status != 'DEFERRED' || this.Bug.status != 'DUPLICATE' ||
+          this.Bug.status != 'REJECTED' || this.Bug.status != 'NOT_A_BUG'
+          || this.Bug.status != 'COULD_NOT_REPRODUCE' || this.Bug.status != 'NEED_MORE_INFO'
+          || this.Bug.status != 'WONT_FIX' || this.Bug.status != 'FIXED')) {
+          alert('Status can be changed only to DEFERRED, DUPLICATE, REJECTED, NOT_A_BUG, COULD_NOT_REPRODUCE, NEED_MORE_INFO, WONT_FIX or FIXED');
+          return;
+        }
+        else if (this.currentStatus == 'VERIFIED' && (this.Bug.status != 'VERIFIED'
+         || this.Bug.status != 'CLOSED')) {
+          alert('Status can be changed only to VERIFIED or CLOSED');
+          return;
+        }
+        else if (this.currentStatus ==
+           'WONT_FIX' && (this.Bug.status != 'WONT_FIX' || this.Bug.status != 'CLOSED')) {
+          alert('Status can be changed only to WONT_FIX or CLOSED');
+          return;
+        }
+        else if (this.currentStatus == 'DUPLICATE' && (this.Bug.status != 'DUPLICATE'
+        || this.Bug.status != 'CLOSED')) {
+          alert('Status can be changed only to DUPLICATE  or CLOSED');
+          return;
+        }
+        else if (this.currentStatus == 'REJECTED' && (this.Bug.status != 'REJECTED'
+         || this.Bug.status != 'CLOSED')) {
+          alert('Status can be changed only to REJECTED or CLOSED');
+          return;
+        }
+        else if (this.currentStatus == 'NOT_A_BUG' && (this.Bug.status != 'NOT_A_BUG' || this.Bug.status != 'CLOSED')) {
+          alert('Status can be changed only to NOT_A_BUG orCLOSED');
+          return;
+        }
+        else if (this.currentStatus == 'COULD_NOT_REPRODUCE' && (this.Bug.status != 'COULD_NOT_REPRODUCE' || this.Bug.status != 'DEFERRED')) {
+          alert('Status can be changed only to DEFERRED');
+          return;
+        }
+        else if (this.currentStatus == 'NEED_MORE_INFO' && (this. Bug.status != 'NEED_MORE_INFO' || this.Bug.status != 'DEFERRED')) {
+          alert('Status can be changed only to NEED_MORE_INFO or DEFERRED');
+          return;
+        }
+        else if (this.currentStatus == 'DEFERRED' && (this.Bug.status != 'DEFERRED' || this.Bug.status != 'ASSIGNED')) {
+          alert('Status can be changed only to DEFERRED or ASSIGNED');
+          return;
+        }
+        else if (this.currentStatus == 'CLOSED' && this.Bug.status != 'CLOSED') {
+          alert('Status can be changed only to CLOSED');
+          return;
+        }
         console.log(error);
         alert('ETA cannot be  a past date')
       //  if(this.bugArray[0]==undefined){
